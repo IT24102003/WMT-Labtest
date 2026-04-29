a26dbaeaf89f15b2f245b865e314f91356a43495
IT24102003 <it24102003@my.sliit.lk>
2026-04-29 22:59:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,29 @@ dotenv.config();
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
+
 
 app.get("/", (req, res) => {
   res.json({ message: "Item Manager API is running..." });
 });
 
+
 app.use("/api/items", itemRoutes);
 
+
 const PORT = process.env.PORT || 5000;
+
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
+    
+    
+    app.listen(PORT, '0.0.0.0', () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
